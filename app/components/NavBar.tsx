543d5fe3e49c7e0e1720b9944176f942aa855479
Elahe Hashemi <elahe6474@gmail.com>
2026-05-05 13:19:17 +0200

    navbar

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function NavBar() {
+  return (
+    <header>
+        <nav>
+            <Link href="/" className={'logo'}>
+                <Image src="/icons/logo.png" width={24} height={24} alt="devEvent logo"/>
+                <p>devEvent</p>
+            </Link>
+            <ul>
+            <Link href="/">home</Link>
+            <Link href="/">events</Link>
+            <Link href="/">create events</Link>
+        </ul>
+        </nav>
+    </header>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,14 @@ export default function Home() {
       <h1 className="text-center"> The Hub for Every Dev <br/> Event You Can't Miss</h1>
       <p className="text-center mt-5">hachathon,meetup and confrances</p>
       <ExploreBtn/>
+      <div className="mt-20 space-y-7">
+        <h3>feature events</h3>
+        <ul className="events"></ul>
+        {[1,2,3,4,5].map((event)=>(
+          <li key={event}>Event {event}</li>
+        ))}
+
+      </div>
     </section>
   );
 }
