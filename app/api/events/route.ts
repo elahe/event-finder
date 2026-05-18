f79ed37499942f8ea5090e6ec51319649c337246
Elahe <elahe6474@gmail.com>
2026-05-18 20:50:42 +0200

    adding cloudinary

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -3,25 +3,55 @@ import { connectToDatabase } from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function POST(req:NextRequest ) {
-   try{
-        await connectToDatabase();
+export async function POST(req: NextRequest) {
+  try {
+    await connectToDatabase();
 
-        const formData = await req.formData()
+    const formData = await req.formData();
+    const event = Object.fromEntries(formData.entries());
 
-        let event
+    const createdEvent = await Event.create(event);
 
-        try{
-            event = Object.fromEntries(formData.entries())
-        }catch(e){
-            return NextResponse.json({message:"invalid json data format"}, {status: 400})
-        }
-
-        const createdEvent= await Event.create(event)
-
-        return NextResponse.json({message:"event created succesfully", event:createdEvent}, {status:201})
-   }catch(e){
-    console.error(e)
-    return NextResponse.json({message:"event creation faild", error: e instanceof Error ? e.message :"unknown"},{status: 500})
-   }
+    return NextResponse.json(
+      {
+        message: "event created successfully",
+        event: createdEvent,
+      },
+      { status: 201 }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      {
+        message: "event creation failed",
+        error: e instanceof Error ? e.message : "unknown",
+      },
+      { status: 500 }
+    );
+  }
 }
+
+// export async function POST(req: NextRequest) {
+//   try {
+//     await connectToDatabase();
+
+//     const event = await req.json();
+
+//     const createdEvent = await Event.create(event);
+
+//     return NextResponse.json(
+//       {
+//         message: "event created successfully",
+//         event: createdEvent,
+//       },
+//       { status: 201 }
+//     );
+//   } catch (e) {
+//     return NextResponse.json(
+//       {
+//         message: "event creation failed",
+//         error: e instanceof Error ? e.message : "unknown",
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
